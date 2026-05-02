76002c9fd7da6d77ef0eae6c79bc8f9423aa7a85
Duncan Everson <duncan.everson@icloud.com>
2026-05-02 13:01:43 -0700

    basic stubbing for server

---
+++ src/backend/server/server.js
@@ -0,0 +1,21 @@
+import express from 'express'
+
+import api from './api/index.js'
+
+const app = express()
+const port = process.env.PORT || 8000
+
+app.use(express.json())
+
+app.use('/', api)
+
+app.use('*splat', function (req, res, next) {
+    res.status(404).send({
+        error: `Requested resource ${req.originalUrl} does not exist`
+    })
+})
+
+
+app.listen(port, function () {
+    console.log("== Server is running on port", port)
+})
