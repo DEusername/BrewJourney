e48b15fe9224180d71f4bf6d29a5863367e35fbe
dotdyl <dotdyldev@gmail.com>
2026-05-02 14:46:59 -0700

    PostgreSQL with Prisma setup
    Setup a PostgreSQL database using Prisma and a docker environment

--- src/backend/server/lib/prisma.js
+++ src/backend/server/lib/prisma.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv"
 import { PrismaPg } from "@prisma/adapter-pg"
-import { PrismaClient } from "../generated/prisma/client.ts"
+import { PrismaClient } from "../db/generated/prisma/client.ts"
 
 dotenv.config({ path: ".env.db" })
 
