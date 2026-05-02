04f845e7fb77602267d5d52840afe0c1966a5442
Duncan Everson <duncan.everson@icloud.com>
2026-05-02 14:06:07 -0700

    added route stubbing into index.js

---
+++ src/backend/server/api/index.js
@@ -0,0 +1,8 @@
+import { Router } from 'express'
+
+// ex for import: import businessesRouter from './businesses.js'
+
+const router = Router()
+// ex for using the router: router.use('/businesses', businessesRouter)
+
+export default router
