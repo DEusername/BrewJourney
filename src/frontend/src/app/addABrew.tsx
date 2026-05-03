870f8e4e545da17b300f239bc5f7448c390cb825
EthanHorizons <ethanbenstar@gmail.com>
2026-05-02 21:28:52 -0700

    started add a brew file

--- src/frontend/src/app/addABrew.tsx
+++ src/frontend/src/app/addABrew.tsx
@@ -1,13 +1,22 @@
 import { Text, View } from "react-native";
 import { StyleSheet } from "react-native";
+import PageHeader from "../components/PageHeader";
+import { FormsDemo } from "../components/Form1";
+import { Input } from "tamagui";
+
 
 export default function AddABrew() {
   return (
     <View style={styles.container}>
-      <View>
-        <Text>Add a Brew Page</Text>
-      </View>
-    </View>
+          <PageHeader title="Add a Brew" />
+          <View style={styles.container}>
+            <FormsDemo />
+
+
+            <Text>Add a Brew Page</Text>
+          </View>
+          
+        </View>
   );
 }
 const styles = StyleSheet.create({
