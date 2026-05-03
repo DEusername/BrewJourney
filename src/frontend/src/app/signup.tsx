4e2c0d0a31adfa1bd76c8db399f4e3bff179dfc7
EthanHorizons <ethanbenstar@gmail.com>
2026-05-03 07:40:26 -0700

    fetch changes

--- src/frontend/src/app/signup.tsx
+++ src/frontend/src/app/signup.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { router } from "expo-router";
 import { View, Text, TextInput, Button, StyleSheet } from "react-native";
+import backend_port from "../environment";
 
 export default function Index() {
     const [firstName, setFirstName] = useState("");
@@ -9,13 +10,37 @@ export default function Index() {
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
 
-  const handleSignup = () => {
+  const handleSignup = async () => {
     setLoading(true);
 
-    setTimeout(() => {
+    const formData = { firstName:firstName, lastName:lastName, email:email, password:password };
+    console.log("FORM DATA:", formData);
+
+    const response = await fetch(`${backend_port}/signup`, {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(formData)
+    })
+
+    if (response.status == 200){
+      // It worked!
+      console.log("Sign up successful!");
+      setTimeout(() => {
       setLoading(false);
       router.replace("/home");
     }, 800);
+    } else {
+      console.log("Trouble signing up :(")
+      setTimeout(() => {
+      setLoading(false);
+      router.replace("/signup");
+    }, 800);
+    }
+
+    // setTimeout(() => {
+    //   setLoading(false);
+    //   router.replace("/home");
+    // }, 800);
   };
 
   return (
