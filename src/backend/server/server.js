11423a163b5f6516d6244f508b81c02c2548740f
dotdyl <dotdyldev@gmail.com>
2026-05-03 05:01:38 -0700

    Reworked brew log display
    Switched over from useContext to useState to allow for the dynamic variables needed for fetching data from the database.

--- src/backend/server/server.js
+++ src/backend/server/server.js
@@ -1,12 +1,12 @@
 import express from 'express'
-
+import cors from 'cors'
 import api from './api/index.js'
 
 const app = express()
 const port = process.env.PORT || 8000
 
 app.use(express.json())
-
+app.use(cors())
 app.use('/', api)
 
 app.use('*splat', function (req, res, next) {
